505e71a67435750322b8ba5cf5d6eeec584eec31
Beila <beilah@nycstudents.net>
2025-10-09 14:01:44 -0400

    no

---
+++ app.js
@@ -0,0 +1,60 @@
+const container = document.querySelector('.container');
+
+const products = [
+  {
+    name: "Syrian Hamster",
+    price: 180,
+    image: "pictures/syrian.png",
+  },
+  {
+    name: "Campbell Dwarf Hamster",
+    price: 200,
+    image: "pictures/campbelldwarf.png",
+  },
+  {
+    name: "Roborovski Dwarf Hamster",
+    price: 225,
+    image: "pictures/roborovskidwarf.png",
+  },
+  { name: "Romanian Hamster", 
+    price: 160, 
+    image: "pictures/romanian.png" },
+  {
+    name: "Russian Dwarf Hamster",
+    price: 225,
+    image: "pictures/russiandwarf.png",
+  },
+  {
+    name: "Chinese Hamster",
+    price: 175,
+    image: "pictures/chinese.png",
+  },
+  {
+    name: "Chinese Striped Hamster",
+    price: 180,
+    image: "pictures/chinesestriped.png",
+  },
+  {
+    name: "Ciscaucasian Hamster",
+    price: 300,
+    image: "pictures/ciscaucasian.png",
+  },
+
+  {
+    name: "Winter White Dwarf Hamster",
+    price: 250,
+    image: "pictures/winterwhitedwarf.png",
+  },
+];
+
+products.forEach(product => {
+    const html = `
+        <div class="card">
+          <h2 class="cardHeader">${product.name}</h2>
+          <img class="img" src=${product.image} alt="Syrian Hamster" />
+          <h3 class="cardPrice">${product.price}</h3>
+          <button class="button">Add to cart</button>
+        </div>
+        `;
+    container.innerHTML += html;
+})
