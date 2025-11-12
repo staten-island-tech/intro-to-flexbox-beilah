69ad013fdcc47058c39d26c8ad0762fcd794fb03
Beila <beilah@nycstudents.net>
2025-11-12 14:02:12 -0500

    Update grocery.js

--- study2/grocery.js
+++ study2/grocery.js
@@ -1,23 +1,26 @@
-function total() {
-  let total = 0;
-cart.forEach((item) => {
-  if(item.price < 5) {
-    total += item.quantity * item.price * 0.95;
-  }
-  else {
-    total += item.quantity * item.price;
-  }
-})
-if (total > 100) {
-  total = total * 0.9;
-}
-    return total;
-}
-
 const cart = [
   { name: "Apples", price: 3.5, quantity: 4 },
   { name: "Milk", price: 4.75, quantity: 2 },
   { name: "Steak", price: 15.99, quantity: 3 },
   { name: "Cereal", price: 5.25, quantity: 1 },
-  { name: "Bananas", price: 1.25, quantity: 6 }
+  { name: "Bananas", price: 1.25, quantity: 6 },
 ];
+
+function total() {
+  let total = 0;
+  cart.forEach((item) => {
+    if (item.price < 5) {
+      total += item.quantity * item.price * 0.95;
+    } else {
+      total += item.quantity * item.price;
+    }
+  });
+  if (total > 100) {
+    total = total * 0.9;
+    console.log(`$${total}`);
+  } else {
+    console.log(`$${total}`);
+  }
+  return total;
+}
+total();
